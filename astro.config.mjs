5b20777cd949c1bf71c9099a6ab6b5ad5999acc6
Jan Shanel <chcjohnie@gmail.com>
2023-01-30 23:16:38 +0100

    More header playing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import UnoCSS from 'unocss/astro'
-
 import sitemap from '@astrojs/sitemap';
+
+import UnoCSS from 'unocss/astro'
 
 // https://astro.build/config
 export default defineConfig({
